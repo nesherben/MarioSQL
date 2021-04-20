b9519c3cd877a41fdf25bca9a438caaf36f1e6d4
nesherben <nestor8299@gmail.com>
2021-04-20 22:20:26 +0200

    Remaked some tables - insert data not avaiable ATM due to new tables

--- MarioSQL_create_2021-04-20_22_14.sql
+++ MarioSQL_create_2021-04-20_22_14.sql
@@ -1,16 +1,27 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2021-03-11 21:21:52.296
+-- Last modification date: 2021-04-20 20:14:01.049
 
 -- tables
+-- Table: Blocks_Level
+CREATE TABLE Blocks_Level (
+    Niveles_Nivel varchar(5) NOT NULL,
+    Bloque_Coord_B varchar(10) NOT NULL,
+    Cantidad integer NOT NULL DEFAULT 0,
+    CONSTRAINT Blocks_Level_pk PRIMARY KEY (Niveles_Nivel,Bloque_Coord_B),
+    CONSTRAINT Blocks_Level_Niveles FOREIGN KEY (Niveles_Nivel)
+    REFERENCES Niveles (Nivel),
+    CONSTRAINT Blocks_Level_Bloque FOREIGN KEY (Bloque_Coord_B)
+    REFERENCES Bloque (Coord_B)
+);
+
 -- Table: Bloque
 CREATE TABLE Bloque (
-    Coord_B varchar(10) NOT NULL,
-    Nivel varchar(5) NOT NULL,
+    Coord_B varchar(10) NOT NULL CONSTRAINT Bloque_pk PRIMARY KEY,
     Tipo varchar(10) NOT NULL,
     Contenido varchar(10) DEFAULT "NO",
-    CONSTRAINT Bloque_pk PRIMARY KEY (Coord_B,Nivel),
-    CONSTRAINT Bloque_Niveles FOREIGN KEY (Nivel)
-    REFERENCES Niveles (Nivel)
+    Cantidad integer NOT NULL DEFAULT 0,
+    CONSTRAINT Bloque_Elemento FOREIGN KEY (Contenido)
+    REFERENCES Elemento (Element_ID)
 );
 
 -- Table: Challenge
@@ -35,14 +46,13 @@ CREATE TABLE Challenge (
 
 -- Table: Clasico
 CREATE TABLE Clasico (
-    Jugador_ID int NOT NULL,
-    Nivel varchar(5) NOT NULL,
+    Jugador_ID int NOT NULL CONSTRAINT Jugador PRIMARY KEY,
+    Nivel varchar(5) NOT NULL DEFAULT New,
     Personaje varchar(10) NOT NULL,
     Vidas int NOT NULL,
     Monedas int NOT NULL,
     Puntuacion int NOT NULL,
     PowerUp int,
-    CONSTRAINT Jugador PRIMARY KEY (Jugador_ID,Nivel),
     CONSTRAINT Clasico_Jugador FOREIGN KEY (Jugador_ID)
     REFERENCES Jugador (Player_ID)
     ON DELETE RESTRICT 
@@ -53,23 +63,42 @@ CREATE TABLE Clasico (
     ON UPDATE CASCADE,
     CONSTRAINT Clasico_Niveles FOREIGN KEY (Nivel)
     REFERENCES Niveles (Nivel)
-    ON DELETE RESTRICT 
+    ON DELETE SET DEFAULT 
+    ON UPDATE CASCADE
+);
+
+-- Table: Elemento
+CREATE TABLE Elemento (
+    Element_ID integer NOT NULL CONSTRAINT Elemento_pk PRIMARY KEY,
+    Nombre varchar(20) NOT NULL,
+    Power integer DEFAULT NULL,
+    CONSTRAINT Elemento_Habilidad FOREIGN KEY (Power)
+    REFERENCES Habilidad (Habilidad_ID)
+    ON DELETE SET DEFAULT 
     ON UPDATE CASCADE
 );
 
 -- Table: Enemigo
 CREATE TABLE Enemigo (
-    Enemy_ID int NOT NULL,
-    Nivel varchar(5) NOT NULL,
+    Enemy_ID int NOT NULL CONSTRAINT Enemigo_pk PRIMARY KEY,
     Nombre varchar(15) NOT NULL,
     Habilidad_ID int,
-    CONSTRAINT Enemigo_pk PRIMARY KEY (Enemy_ID,Nivel),
     CONSTRAINT Enemigo_Habilidad FOREIGN KEY (Habilidad_ID)
     REFERENCES Habilidad (Habilidad_ID)
     ON DELETE SET DEFAULT 
-    ON UPDATE CASCADE,
-    CONSTRAINT Enemigo_Niveles FOREIGN KEY (Nivel)
-    REFERENCES Niveles (Nivel)
+    ON UPDATE CASCADE
+);
+
+-- Table: Enemy_Level
+CREATE TABLE Enemy_Level (
+    Niveles_Nivel varchar(5) NOT NULL,
+    Enemigo_Enemy_ID int NOT NULL,
+    Cantidad integer NOT NULL DEFAULT 0,
+    CONSTRAINT Enemy_Level_pk PRIMARY KEY (Niveles_Nivel,Enemigo_Enemy_ID),
+    CONSTRAINT Enemy_Level_Niveles FOREIGN KEY (Niveles_Nivel)
+    REFERENCES Niveles (Nivel),
+    CONSTRAINT Enemy_Level_Enemigo FOREIGN KEY (Enemigo_Enemy_ID)
+    REFERENCES Enemigo (Enemy_ID)
 );
 
 -- Table: Fortuna
@@ -102,7 +131,6 @@ CREATE TABLE Jugador (
 -- Table: Niveles
 CREATE TABLE Niveles (
     Nivel varchar(5) NOT NULL CONSTRAINT Niveles_pk PRIMARY KEY,
-    Mundo varchar(20) NOT NULL,
     Check_flag boolean NOT NULL,
     Tiempo int NOT NULL
 );
--- MarioSQL_drop_2021-04-20_22_14.sql
+++ MarioSQL_drop_2021-04-20_22_14.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2021-03-11 21:21:52.296
+-- Last modification date: 2021-04-20 20:14:01.049
 
 -- foreign keys
 -- ALTER TABLE DROP CONSTRAINT not supported
@@ -24,14 +24,28 @@
 
 -- ALTER TABLE DROP CONSTRAINT not supported
 
+-- ALTER TABLE DROP CONSTRAINT not supported
+
+-- ALTER TABLE DROP CONSTRAINT not supported
+
+-- ALTER TABLE DROP CONSTRAINT not supported
+
+-- ALTER TABLE DROP CONSTRAINT not supported
+
 -- tables
+DROP TABLE Blocks_Level;
+
 DROP TABLE Bloque;
 
 DROP TABLE Challenge;
 
 DROP TABLE Clasico;
 
+DROP TABLE Elemento;
+
 DROP TABLE Enemigo;
+
+DROP TABLE Enemy_Level;
 
 DROP TABLE Fortuna;
 
